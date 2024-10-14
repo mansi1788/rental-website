b0f2faeaa5f28134e1c8216486783d99913c82c3
mansi.1788 <maithanimansi8@gmail.com>
2024-10-14 17:02:04 +0530

    "productdetails updated"

--- frontend/src/components/productdetail.jsx
+++ frontend/src/components/productdetail.jsx
@@ -1,22 +1,19 @@
-import React, { useState,useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { FavoritesContext } from '../context/FavoritesContext'; // Import FavoritesContext
 import { CartContext } from '../context/Cartcontext';
 import { useNavigate, useParams } from 'react-router-dom';
-import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom'; 
 import axios from 'axios';
 
-function len(d){
-
+function len(d) {
   const words = d.split("");
-  if(words.length>30){
-  return words.slice(0,30).join("")+"...";
+  if (words.length > 30) {
+    return words.slice(0, 30).join("") + "...";
+  }
+  return d;
 }
 
-return d;
-  }
-
-const ProductDetail = ({ products = [], userId  }) => {
+const ProductDetail = ({ products = [], userId }) => {
   const { id } = useParams();
   const { cart, addToCart } = useContext(CartContext);
   const { favorites, addToFavorites } = useContext(FavoritesContext); // Use favorites context
@@ -35,13 +32,11 @@ const ProductDetail = ({ products = [], userId  }) => {
   const isProductInCart = cart.some((cartItem) => cartItem.id === product.id);
   const isProductInFav = favorites.some((favItem) => favItem.id === product.id); 
 
-  
-
   const handleAddToCart = async () => {
     try {
       const response = await axios.post('http://localhost:8000/api/cart', {
-        userId,
-        productId:product.id,
+        userId: product.id || localStorage.getItem('id'),
+        productId: product.id,
         quantity,
       });
 
@@ -59,84 +54,84 @@ const ProductDetail = ({ products = [], userId  }) => {
   const similarProducts = products.filter(
     (prod) => prod.category === category && prod.id !== product.id
   );
+
   return (
     <>
-    <div className="container mx-auto p-4 mt-40">
-      <div className="flex flex-col md:flex-row items-center">
-        <div className="md:w-1/2">
-          <img
-            src={image || 'https://via.placeholder.com/400'}
-            className="img-fluid rounded-lg shadow-lg w-96 h-96"
-            alt={title}
-          />
-        </div>
-        <div className="md:w-1/2 md:pl-8">
-          <h2 className="mt-3 text-4xl font-semibold">{title}</h2>
+      <div className="container mx-auto p-4 mt-40">
+        <div className="flex flex-col md:flex-row items-center">
+          <div className="md:w-1/2">
+            <img
+              src={image || 'https://via.placeholder.com/400'}
+              className="img-fluid rounded-lg shadow-lg w-96 h-96"
+              alt={title}
+            />
+          </div>
+          <div className="md:w-1/2 md:pl-8">
+            <h2 className="mt-3 text-4xl font-semibold">{title}</h2>
 
-          <div>
+            <div>
+              <button
+                onClick={() => addToFavorites(product)} // Add to favorites
+                className={`mt-4 bg-red-600 text-white py-2 px-6 rounded-lg shadow-lg
+                 hover:bg-red-400 transition duration-300 
+                 ${isProductInFav ? 'cursor-not-allowed opacity-50' : ''}`}
+                disabled={isProductInFav}
+              >
+                <i className="fa-regular fa-heart"></i>
+                {isProductInFav ? 'Added to Favourite' : 'Add to Favourite'}
+              </button>
+            </div>
+
+            <p className="mt-4 text-gray-800">{description}</p>
+            <h4 className="mt-6 text-2xl font-bold text-blue-600">
+              Price: <i className="fa fa-inr" aria-hidden="true"> {price * 50}</i>
+            </h4>
+
             <button
-              onClick={() => addToFavorites(product)} // Add to favorites
-              className={`mt-4 bg-red-600 text-white py-2 px-6 rounded-lg shadow-lg
-               hover:bg-red-400 transition duration-300 
-               ${isProductInCart ? 'cursor-not-allowed opacity-50' : ''}`}
-              disabled={isProductInFav}
+              onClick={handleAddToCart}
+              className={`mt-4 bg-blue-600 text-white py-2 px-6 rounded-lg shadow-lg
+               hover:bg-blue-700 transition duration-300 ${
+                isProductInCart ? 'cursor-not-allowed opacity-50' : ''
+              }`}
+              disabled={isProductInCart}
             >
-            
-              <i className="fa-regular fa-heart"></i> 
-            
-              {isProductInFav ? 'Added to Favourite' : 'Add to Favourite'}
+              {isProductInCart ? 'Already in Cart' : 'Add to Cart'}
+            </button>
+
+            <button className="mt-4 bg-blue-600 ml-10 text-white py-2 px-6 rounded-lg shadow-lg hover:bg-blue-700 transition duration-300">
+              Buy Now
             </button>
           </div>
-
-          {/* <p className="text-gray-600 text-lg mt-1">
-            Category: <span className="font-bold">{category}</span>
-          </p> */}
-          <p className="mt-4 text-gray-800">{description}</p>
-          <h4 className="mt-6 text-2xl font-bold text-blue-600">Price: <i class="fa fa-inr" aria-hidden="true"> {product.price*50}</i></h4>
-
-          <button
-            onClick={handleAddToCart}
-            className={`mt-4 bg-blue-600 text-white py-2 px-6 rounded-lg shadow-lg
-             hover:bg-blue-700 transition duration-300 ${
-              isProductInCart ? 'cursor-not-allowed opacity-50' : ''
-            }`}
-            disabled={isProductInCart}
-          >
-            {isProductInCart ? 'Already in Cart' : 'Add to Cart'}
-          </button>
-
-          <button className="mt-4 bg-blue-600 ml-10 text-white py-2 px-6 rounded-lg shadow-lg hover:bg-blue-700 transition duration-300">
-            Buy Now
-          </button>
         </div>
       </div>
-    </div>
 
-<div>
-<h1 className='text-6xl text-center font-serif mt-14 mb-6'>SIMILAR PRODUCTS</h1>
+      <div>
+        <h1 className="text-6xl text-center font-serif mt-14 mb-6">SIMILAR PRODUCTS</h1>
 
-<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-8 bg-orange-200'>
-  {similarProducts.slice(0, 8).map((product) => (
-    <Link to={`/product/${product.id}`} key={product.id}>
-      <div className='flex flex-col items-center bg-white p-4 shadow-xl rounded-lg
-       transform transition-transform duration-500 hover:scale-105 hover:shadow-2xl hover:rotate-3'>
-        <img
-          src={product.image}
-          alt={product.title}
-          className='w-40 h-40 mb-4 object-contain'
-        />
-        <h3 className='text-lg font-bold mb-2'>{len(product.title)}</h3>
-        <p className='text-gray-600 text-xl font-semibold mb-4'><i class="fa fa-inr" aria-hidden="true"> {product.price*50}</i></p>
-        <button className='bg-blue-600 text-white py-2 px-4 rounded-lg text-sm font-semibold hover:bg-blue-700 transition-colors duration-300'>
-          Add to Cart
-        </button>
-      </div>      
-    </Link>
-  ))}
-</div>
-</div>
-<div className='mt-3'></div>
-</>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-8 bg-orange-200">
+          {similarProducts.slice(0, 8).map((product) => (
+            <Link to={`/product/${product.id}`} key={product.id}>
+              <div className="flex flex-col items-center bg-white p-4 shadow-xl rounded-lg
+               transform transition-transform duration-500 hover:scale-105 hover:shadow-2xl hover:rotate-3">
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  className="w-40 h-40 mb-4 object-contain"
+                />
+                <h3 className="text-lg font-bold mb-2">{len(product.title)}</h3>
+                <p className="text-gray-600 text-xl font-semibold mb-4">
+                  <i className="fa fa-inr" aria-hidden="true"> {product.price * 50}</i>
+                </p>
+                <button className="bg-blue-600 text-white py-2 px-4 rounded-lg text-sm font-semibold hover:bg-blue-700 transition-colors duration-300">
+                  Add to Cart
+                </button>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="mt-3"></div>
+    </>
   );
 };
 
